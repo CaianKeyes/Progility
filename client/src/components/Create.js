46ada1a33ca3696f63fd934de234cc3c6029da16
Caian Keyes <caiankeyes@gmail.com>
2023-06-08 08:52:41 +0100

    feat: Create Task form completed

--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -5,9 +5,25 @@ function Create() {
   const [timeSpan, setTimespan] = useState('');
   const [description, setDescription] = useState('');
   const [requirments, setRequirments] = useState('');
+  const [requirmentsList, setRequirmentsList] = useState([]);
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+
+    setRequirmentsList([...requirmentsList, requirments]);
+    setRequirments('');
+  }
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+
+    if (!title || !timeSpan) {
+      alert('Title and Timespan are required feilds');
+    } 
+  }
 
   return <>
-    <form className="register">
+    <form className="register" onSubmit={handleSubmit}>
       <h2 className="form title">Create Task</h2>
       <h3 className='form'>Title:</h3>
         <input 
@@ -16,6 +32,39 @@ function Create() {
           onChange={e => setTitle(e.target.value)}
           className='form input'
         ></input>
+
+        <h3 className='form'>Description:</h3>
+        <input 
+          type='text' 
+          value={description || ''}
+          onChange={e => setDescription(e.target.value)}
+          className='form input'
+        ></input>
+
+        <h3 className='form'>Timespan:</h3>
+        <input 
+          type='number' 
+          value={timeSpan || ''}
+          onChange={e => setTimespan(e.target.value)}
+          className='form number'
+        ></input>
+
+        <h3 className='form'>Requirments:</h3>
+        <input 
+          type='text' 
+          value={requirments || ''}
+          onChange={e => setRequirments(e.target.value)}
+          className='form input'
+        ></input>
+        <button className="form add" onClick={handleAdd}>Add</button>
+
+        <ul>
+          {requirmentsList.map(item => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+
+        <button className='form submit' type='submit'>Create</button>
     </form>
   </>
 }
