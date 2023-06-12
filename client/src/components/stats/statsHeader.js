ccddf63da603dee0432707cc3809fdf793a23237
Caian Keyes <caiankeyes@gmail.com>
2023-06-12 08:35:34 +0100

    feat: improved styling for stats header

--- client/src/components/stats/statsHeader.js
+++ client/src/components/stats/statsHeader.js
@@ -5,8 +5,8 @@ function StatsHeader ({onData}) {
   }
 
   return <>
-  <form>
-    <h2>Select Stats:</h2>
+  <form className='stats_header'>
+    <h2 className='form' >Select Stats:</h2>
     <select className="dropdown" onChange={handleChange}>
       <option value='1a'>Group - All Time</option>
       <option value='2a'>Group - Monthly</option>
